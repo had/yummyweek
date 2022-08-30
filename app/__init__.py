0ada725873abed41cd5bc9fffbf611b5b8aa07ab
Hadrien Hamel <hadrien.hamel@gmail.com>
2022-08-30 10:28:31 +0200

    Replaced dependency on flask-fontawesome on direct link to CDN (more icons available)

--- app/__init__.py
+++ app/__init__.py
@@ -1,13 +1,11 @@
 from flask import Flask
 from flask_bootstrap import Bootstrap4
-from flask_fontawesome import FontAwesome
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 from config import config
 
 db = SQLAlchemy()
 bootstrap = Bootstrap4()
-fa = FontAwesome()
 migrate = Migrate()
 
 
@@ -16,7 +14,6 @@ def create_app(config_name):
     app.config.from_object(config[config_name])
 
     bootstrap.init_app(app)
-    fa.init_app(app)
     db.init_app(app)
     migrate.init_app(app, db, render_as_batch=True)
 
