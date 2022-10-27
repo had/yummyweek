e3434da14c8b61e8cb467b0731b3dc01536102f9
Hadrien Hamel <hadrien.hamel@gmail.com>
2022-10-27 09:06:14 +0200

    Add possibility to add a dish (no composed meal, no recipes, not yet)

--- app/meals/forms.py
+++ app/meals/forms.py
@@ -1,15 +1,17 @@
 from flask_wtf import FlaskForm
 from flask_wtf.file import FileRequired, FileAllowed
-from wtforms import StringField, SelectField, IntegerField, FileField, SubmitField
+from wtforms import StringField, SelectField, IntegerField, FileField, SubmitField, SelectMultipleField
 from wtforms.validators import DataRequired
 
 
-class MealElementForm(FlaskForm):
+class DishForm(FlaskForm):
     name = StringField("Name", validators=[DataRequired()])
-    category = SelectField("Category", validators=[DataRequired()])
     prep_time = IntegerField("Preparation time (minutes)", validators=[DataRequired()], default=0)
     cooking_time = IntegerField("Preparation time (minutes)", validators=[DataRequired()], default=0)
     periodicity = IntegerField("Min. days before eating again", validators=[DataRequired()], default=0)
+    moment = SelectField("Moment", validators=[DataRequired()], choices=["-", "Lunch", "Dinner", "Both"])
+    elements = SelectMultipleField("Elements", choices=[])
+    submit = SubmitField("Add")
 
 
 class DishesUploadForm(FlaskForm):
--- app/meals/views.py
+++ app/meals/views.py
@@ -1,4 +1,5 @@
 import os
+import re
 
 from flask import render_template, redirect, url_for, flash, current_app
 from flask_login import login_required
@@ -7,9 +8,10 @@ from werkzeug.datastructures import FileStorage
 from werkzeug.utils import secure_filename
 
 from . import meals
-from .forms import DishesUploadForm
+from .forms import DishesUploadForm, DishForm
 from .dao import get_all_meals, MealsDBAccess
 from .dao_xlsx_readers import XlsxReader
+from .models import Dish
 from .. import db
 
 
@@ -17,6 +19,37 @@ from .. import db
 def list_meals():
     upload_form = DishesUploadForm()
     return render_template("meals.html", meals=get_all_meals().values(), upload_form=upload_form)
+
+
+@meals.route("/meals/add/<dish_type>", methods=["GET", "POST"])
+def add_dish(dish_type: str):
+    nav_elts = {
+        "dish": "Dish",
+        # "add_composed_meal": "Composed meal"
+    }
+    dish_form = DishForm()
+    if dish_form.validate_on_submit():
+        dish_name: str = dish_form.name.data.strip()
+        dish_id: str = re.sub("[^A-Z0-9]", "_", dish_name.upper())
+        dish = Dish(
+            id=dish_id,
+            name=dish_name,
+            category=dish_form.moment.data,
+            prep_time_m=dish_form.prep_time.data,
+            cooking_time_m=dish_form.cooking_time.data,
+            periodicity_d=dish_form.periodicity.data
+        )
+        try:
+            db.session.add(dish)
+            db.session.commit()
+            flash(f"{dish.name} added")
+            return redirect(url_for(".list_meals"))
+        except IntegrityError as e:
+            db.session.rollback()
+    if dish_type == "dish":
+        del dish_form.elements
+    return render_template("add_dish.html", nav_elts=nav_elts, current_nav=dish_type, dish_form=dish_form)
+
 
 
 @meals.route("/meals/upload", methods=["POST"])
